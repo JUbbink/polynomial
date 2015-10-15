b82680d7fa4c252a249c368b0984a7a8ac4b87fd
hank3150 <hank31500@gmail.com>
2015-10-15 17:27:04 +0200

    refactor commit
    I cant refactor these changes via netbeans so this is via the github
    desktop client

--- src/polynomial/Polynomial.java
+++ src/polynomial/Polynomial.java
@@ -15,39 +15,24 @@ import java.util.Comparator;
  */
 public class Polynomial {
     ArrayList<Integer> order = new ArrayList<Integer>();
-    ArrayList<Integer> coefficient = new ArrayList<Integer>();
-    Integer[][] combined;
-    
-    public Polynomial(){
-        //constructor
-    }
+    ArrayList<Integer> quotient = new ArrayList<Integer>();
+    int[][] polynomial;
     
     public void addOrder(int num){
         order.add(num);
     }
     
-    public Integer getOrder(int num){
-        return combined[num][1];
+    public void addQuotient(int num){
+        quotient.add(num);
     }
-    
-    public void addCoefficient(int num){
-        coefficient.add(num);
-    }
-    
-    public Integer getCoefficient(int num){
-        return combined[num][0];
-    }
-    //create a 2d array from the 2 arraylist such that the first collumn contains the coefficient and the second collumn the order
-    //the array is also sorted on the order from highest to lowest
-    public void create(){
-        combined = new Integer [order.size()][2];
-        for (int i = 0; i < order.size(); i++){
-            int num = Integer.parseInt(coefficient.get(i).toString());
-            combined[i][0] = num;
-            num = Integer.parseInt(order.get(i).toString());
-            combined[i][1] = num;
+    public void create(ArrayList a, ArrayList b){
+        polynomial = new int [a.size()][2];
+        for (int i = 0; i < a.size(); i++){
+            int num = Integer.parseInt(a.get(i).toString());
+            polynomial[i][0] = num;
+            num = Integer.parseInt(b.get(i).toString());
+            polynomial[i][1] = num;
         }
-        mysort(combined);
     }
     public static Integer[][] mysort(Integer[][] ar) {
         Arrays.sort(ar, new Comparator<Integer[]>() {
@@ -55,7 +40,7 @@ public class Polynomial {
             public int compare(Integer[] int1, Integer[] int2) {
                 Integer numOfKeys1 = int1[1];
                 Integer numOfKeys2 = int2[1];
-                return numOfKeys2.compareTo(numOfKeys1);
+                return numOfKeys1.compareTo(numOfKeys2);
             }
         });
         return ar;
