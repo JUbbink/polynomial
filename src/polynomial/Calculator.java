7913dabe86c760aca1882b7132c07b8dc9b4b5cb
hank3150 <hank31500@gmail.com>
2015-10-15 21:44:07 +0200

    5. added a working addition method

--- src/polynomial/Calculator.java
+++ src/polynomial/Calculator.java
@@ -50,6 +50,9 @@ public class Calculator {
             System.out.println("ERROR, polynomials undefined");
         }
         */
+        /*
+        so version 2 is also fucked, jesus christ
+        
         if (poly1.combined.length > poly2.combined.length){
             for(int i = 0; i < poly1.combined.length; i++){
                 for(int x = 0; x<poly1.combined.length; x++){
@@ -57,14 +60,22 @@ public class Calculator {
                         add.setCoefficient(poly1.getCoefficient(i)+poly2.getCoefficient(x));
                         add.setOrder(poly1.getOrder(i));
                     }
+                    else{
+                        add.setCoefficient(poly1.getCoefficient(i));
+                        add.setOrder(poly1.getOrder(i));
+                    }
                 }
             }
         }
         else if(poly1.combined.length < poly2.combined.length){
-            for(int i = 0; i < poly2.combined.length; i++){
+            for(int i = 0; i < poly1.combined.length; i++){
                 for(int x = 0; x<poly2.combined.length; x++){
                     if (poly2.getOrder(i) == poly1.getOrder(x)){
                         add.setCoefficient(poly2.getCoefficient(i)+poly1.getCoefficient(x));
+                        add.setOrder(poly2.getOrder(i));
+                    }
+                    else{
+                        add.setCoefficient(poly2.getCoefficient(i));
                         add.setOrder(poly2.getOrder(i));
                     }
                 }
@@ -73,10 +84,84 @@ public class Calculator {
         else{
             System.out.println("ERROR, polynomials undefined");
         }
+        */
+        /*
+        int count = 0;
+        //if the length of both polynomials are the same this 
+        if (poly1.combined.length == poly2.combined.length){
+            for (int i = 0; i < poly1.combined.length && i < poly2.combined.length; i++){
+
+                if (poly1.getOrder(i) > poly2.getOrder(i)){
+                    add.setOrder(poly1.getOrder(i));
+                    add.setCoefficient(poly1.getCoefficient(i));                
+                }
+                else if (poly1.getOrder(i) < poly2.getOrder(i)){
+                    add.setOrder(poly2.getOrder(i));
+                    add.setCoefficient(poly2.getCoefficient(i));  
+                }
+                else if (poly1.getOrder(i) == poly2.getOrder(i)){
+                    add.setOrder(poly1.getOrder(i));
+                    add.setCoefficient(poly1.getCoefficient(i)+ poly2.getCoefficient(i));
+                }
+                else{
+                    System.out.println("ERROR, polynomial undefined");
+                }   
+            }
+            add.create();
+            return add;
+        }
+        if (poly1.getOrder(0) < poly2.getOrder(0)){
+            count = poly2.getOrder(0);
+        }
+        else if (poly1.getOrder(0) > poly2.getOrder(0)){
+            count = poly1.getOrder(0);
+        }
+        int num1 = 0;
+        int num2 = 0;
+        //Version 3, same as the last 2
+        //this loop evaluates whether the exponent as defined in count appears in either array, if it does it adds that exponent to the new polynomial
+        //if the exponent as defined in count appears in both arrays
+        while( count > 0){
+            if(count == poly1.getOrder(num1) && count == poly2.getOrder(num2)){
+                add.setOrder(poly1.getOrder(count));
+                add.setCoefficient(poly1.getCoefficient(count)+ poly2.getCoefficient(count));
+                count--;
+                num1++;
+                num2++;
+            }
+            else if(count == poly1.getOrder(num1)){
+                add.setOrder(poly1.getOrder(count));
+                add.setCoefficient(poly1.getCoefficient(count));
+                count--;
+                num1++;                    
+            }
+            else if(count == poly2.getOrder(num2)){
+                add.setOrder(poly2.getOrder(count));
+                add.setCoefficient(poly2.getCoefficient(count));
+                count--;
+                num2++; 
+            }
+            else if(count != poly1.getOrder(num1) && count != poly2.getOrder(num2)){
+                count--;
+            }
+        }
+                */
         
+        for (int i = 0; i < poly1.combined.length; i++){
+            add.setOrder(poly1.getOrder(i));
+            add.setCoefficient(poly1.getCoefficient(i));
+        }
+        for (int i = 0; i < poly2.combined.length; i++){
+            for(int x = 0; x < poly1.combined.length; x++){
+                if (poly2.getOrder(i) == poly1.getOrder(x)){
+                    add.editCoefficient(i, poly2.getCoefficient(i));
+                    break;
+                }
+            }
+            add.setOrder(poly2.getOrder(i));
+            add.setCoefficient(poly2.getCoefficient(i));
+        }
         add.create();
         return add;
-    }
-    
-    
+    }   
 }
