94d9a6bfc037ad18cd3b1db3575049748c4e1895
hank3150 <hank31500@gmail.com>
2015-10-15 18:32:50 +0200

    4. Added a method addition() to class calculator which returns a polynomial c where c = a+b

--- src/polynomial/Polynomial.java
+++ src/polynomial/Polynomial.java
@@ -16,43 +16,45 @@ import java.util.Comparator;
 public class Polynomial {
     ArrayList<Integer> order = new ArrayList<Integer>();
     ArrayList<Integer> coefficient = new ArrayList<Integer>();
-    Integer[][] combined;
+    int[][] combined;
     
     public Polynomial(){
         //constructor
     }
     
-    public void addOrder(int num){
+    public void setOrder(int num){
         order.add(num);
     }
     
-    public Integer getOrder(int num){
+    public int getOrder(int num){
         return combined[num][1];
     }
     
-    public void addCoefficient(int num){
+    public void setCoefficient(int num){
         coefficient.add(num);
     }
     
-    public Integer getCoefficient(int num){
+    public int getCoefficient(int num){
         return combined[num][0];
     }
     //create a 2d array from the 2 arraylist such that the first collumn contains the coefficient and the second collumn the order
     //the array is also sorted on the order from highest to lowest
+    //you NEED to call this method otherwise the polynomial won't be created, you will just have 2 unconnected arraylists
     public void create(){
-        combined = new Integer [order.size()][2];
+        combined = new int [order.size()][2];
         for (int i = 0; i < order.size(); i++){
-            int num = Integer.parseInt(coefficient.get(i).toString());
+            int num = coefficient.get(i);
             combined[i][0] = num;
-            num = Integer.parseInt(order.get(i).toString());
+            num = order.get(i);
             combined[i][1] = num;
         }
         mysort(combined);
     }
-    public static Integer[][] mysort(Integer[][] ar) {
-        Arrays.sort(ar, new Comparator<Integer[]>() {
+    //sorts the newly created array on the order collumn, don't call this method seperately
+    public static int[][] mysort(int[][] ar) {
+        Arrays.sort(ar, new Comparator<int[]>() {
             @Override
-            public int compare(Integer[] int1, Integer[] int2) {
+            public int compare(int[] int1, int[] int2) {
                 Integer numOfKeys1 = int1[1];
                 Integer numOfKeys2 = int2[1];
                 return numOfKeys2.compareTo(numOfKeys1);
