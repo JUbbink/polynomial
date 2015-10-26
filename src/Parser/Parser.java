d22532bd33a0843c014de67b4fdae267ad5e96d8
JUbbink <ubbink.jeroen@gmail.com>
2015-10-26 23:37:07 +0100

    Parser fixed, doing math now

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -32,79 +32,177 @@ import java.util.HashMap;
  */
 public class Parser {
     
-    public void main(String args[]){
-        Scanner s = new Scanner(System.in);
-        Parser(s);
+    public static void main(String args[]){
+        Parser p = new Parser();
+        p.Parser();
     }
     
-    public void Parser(Scanner s){
-        String l ="";
-        
-        while(s.hasNext() == true){   //parsing input
-            l = s.nextLine();
-        }
-        String[] parts = l.split(" ");//this gets me an array of all the separate elements
-        //List<String> items = Arrays.asList(str.split("\\s*,\\s*"));
+    public void Parser(){
+        String s = "";
+        System.out.println("Press Start:");
+        Scanner scan = new Scanner(System.in);
+        s = scan.nextLine();
+        System.out.println(s);
 
+        String[] parts = s.split(" ");//this gets me an array of all the separate elements
         
-        if(parts.length == 5){//either case 4 or 5
-            int prime = Integer.parseInt(parts[3]);
-            Polynomial poly1 = stringToPoly(parts[1],prime);
-            Polynomial poly2 = stringToPoly(parts[2],prime);
-            
-            if(parts[4].equals("div")){//case 5
-                //do division
-                poly1.divide(poly2);
-                System.out.println(poly1.parsePoly());
+        // three polynomials, where you decide if poly1 = poly2 mod poly3
+        // case1 is: "poly" "polymod" poly1 poly2 poly3
+
+        // two polynomials mod prime, where you do sum, difference, product
+        // case2 is: "poly" "basic" poly1 poly2 prime
+        
+        // a multiplication coefficient , and one polynomial mod prime where you do the scalar multiplication
+        // case3 is: "poly" "scalar" poly1 coefficient prime
+ 
+        // two polynomials, mod prime, do xGCD
+        // case4 is: "poly" "GCD" poly1 poly2 prime
+        
+        // two polynomials, mod prime, do long division (return quotient and remainder)
+        // case5 is: "poly" "division" poly1 poly2 prime
+        
+        if(parts.length>0){
+            if(parts[0].equals("poly")){
+                if(parts[1].equals("polymod")){
+                    try{
+                        Polynomial poly1 = stringToPoly(parts[2]);
+                        Polynomial poly2 = stringToPoly(parts[3]);
+                        Polynomial poly3 = stringToPoly(parts[4]);
+                        
+                        //do math
+                        System.out.println("Not yet implemented.");
+                    }
+                    catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+
+                }
+                else if(parts[1].equals("basic")){
+                    try{
+                        Polynomial poly1 = stringToPoly(parts[2]);
+                        Polynomial poly2 = stringToPoly(parts[3]);
+
+                        int prime = Integer.parseInt(parts[4]);
+
+                        Polynomial sum = poly1.add(poly2);
+                        System.out.println("Sum: "+sum.parsePoly());
+
+                        Polynomial difference = poly1.subtract(poly2);
+                        System.out.println("Difference: "+difference.parsePoly());
+
+                        Polynomial product = poly1.multiply(poly2);
+                        //System.out.println("Product: "+product.parsePoly());
+                        System.out.println("Not yet implemented.");
+                    }
+                    catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                }
+                else if(parts[1].equals("scalar")){
+                    try{
+                        int prime = Integer.parseInt(parts[4]);
+                        Polynomial poly1 = stringToPoly(parts[2]);
+                        poly1.setModulo(prime);
+                        int scalar = Integer.parseInt(parts[3]);
+
+                        Polynomial poly2 = poly1.scalar(scalar);
+                        System.out.println("Scalar: "+poly2.parsePoly());
+                    }
+                    catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                }
+                else if(parts[1].equals("GCD")){
+                    try{
+                        Polynomial poly1 = stringToPoly(parts[2]);
+                        Polynomial poly2 = stringToPoly(parts[3]);
+                        int prime = Integer.parseInt(parts[4]);
+                        poly1.setModulo(prime);
+                        poly2.setModulo(prime);
+
+                        int[] gcdOutput = poly1.gcd(poly2);
+    //                    System.out.println("GCD: "+gcdOutput[0]+","+gcdOutput[1]);
+                        System.out.println("Not yet implemented.");
+                        int[] xGCDOutput = poly1.xGCD(poly2);
+    //                    System.out.println("xGCD: "+xGCDOutput[0]+","+xGCDOutput[1]);
+                        System.out.println("Not yet implemented.");
+                    }
+                    catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                }
+                else if(parts[1].equals("division")){
+                    try{
+                        Polynomial poly1 = stringToPoly(parts[2]);
+                        Polynomial poly2 = stringToPoly(parts[3]);
+                        int prime = Integer.parseInt(parts[4]);
+                        poly1.setModulo(prime);
+                        poly2.setModulo(prime);
+
+                        Polynomial poly3 = poly1.divide(poly2);
+                        //System.out.println("Division: "+poly3.parsePoly());
+                        System.err.println("Not yet implemented.");
+                    }
+                    catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                }
+                else{
+                    System.out.println("Please make a valid selection.");
+                }
             }
-            else if(parts[4].equals("gcd")){//case 4
-                //do gcd
-                poly1.gcd(poly2);
-                System.out.println(poly1.parsePoly());
-                poly1.xGCD(poly2);
-                System.out.println(poly1.parsePoly());
+            else if(parts[0].equals("FF")){
+                System.out.println("This functionality is not implemented yet.");
+            }
+            else{
+                System.out.println("Please make a valid selection.");
             }
         }
-        else if(parts.length==4){//remaining three cases
-            
-            if(verifyPart(parts[1]) && verifyPart(parts[2]) && verifyPart(parts[3])){//contains a polynomial, thus case1
-                Polynomial poly1 = stringToPoly(parts[1],1);
-                Polynomial poly2 = stringToPoly(parts[2],1);
-                Polynomial poly3 = stringToPoly(parts[3],1);
-                
-                //do math
-            }
-                
-            else if(verifyPart(parts[1]) && !verifyPart(parts[2]) && !verifyPart(parts[3])){//is a coefficient, thus case 3
-                int coefficient = Integer.parseInt(parts[2]);
-                int prime = Integer.parseInt(parts[3]);
-                Polynomial poly1 = new Polynomial(prime);
-                
-                poly1.scalar(coefficient);
-                System.out.println(poly1.parsePoly());
-            }
-                
-            else if(verifyPart(parts[1]) && verifyPart(parts[2]) && !verifyPart(parts[3])){//only case 2 remains
-                int prime = Integer.parseInt(parts[3]);
-                Polynomial poly1 = stringToPoly(parts[1],prime);
-                Polynomial poly2 = stringToPoly(parts[2],prime);
-                
-                
-                poly1.add(poly2);
-                System.out.println(poly1.parsePoly());
-                
-                poly1 = stringToPoly(parts[1],prime);
-                poly2 = stringToPoly(parts[2],prime);
-                poly1.subtract(poly2);
-                System.out.println(poly1.parsePoly());
-                
-                poly1 = stringToPoly(parts[1],prime);
-                poly2 = stringToPoly(parts[2],prime);
-                poly1.multiply(poly2);
-                System.out.println(poly1.parsePoly());
-            }
+        else{
+            System.out.println("Please enter a valid command.");
         }
-    }    
+    }
+    
+    private int getNumberOfDigits(String s){
+        int j = 0;
+        while(j < s.length() && Character.isDigit(s.charAt(j))) 
+            j++;
+        return j;
+    }
+    
+    public Polynomial stringToPoly(String input) throws Exception{
+        Map<Integer,Integer> map = new HashMap();
+        
+        if(Character.isDigit(input.charAt(0))){
+            input = "+"+input;
+        }
+        while(!input.equals("")){
+            int sign = 1;
+            if(input.substring(0, 1).equals("-")){
+                sign = -1;
+            }
+            input = input.substring(1);
+            int numberOfDigits = getNumberOfDigits(input);
+            int coef = sign*Integer.parseInt(input.substring(0,numberOfDigits));
+            input = input.substring(numberOfDigits);
+            if(!(input.substring(0,2).equals("X^")) && !(input.substring(0,2).equals("x^"))){
+                throw new Exception("No X^ found.");
+            }
+            input = input.substring(2);
+            if(input.substring(0, 1).equals("-")){
+                input = input.substring(1);
+                sign = -1;
+            }
+            else 
+                sign = 1;
+            numberOfDigits = getNumberOfDigits(input);
+            int exp = sign*Integer.parseInt(input.substring(0,numberOfDigits));
+            input = input.substring(numberOfDigits);
+            map.put(exp, coef);
+        }
+        return new Polynomial(map);
+    }
+        
         //prime number p and irreducible polynomial, return addition and multiplication table
         //case1 is: "FF" prime poly
         
@@ -121,90 +219,4 @@ public class Parser {
         //case5 is: "FF" poly prime
 //        }
 //    }
-    
-    private boolean verifyPart(String s) {
-        Scanner stringScanner = new Scanner(s);
-
-        /* Check if there is a coefficient */
-        if(!(stringScanner.hasNextInt())) {
-            return false;
-        } else {
-            /* Consume int */
-            stringScanner.nextInt();
-        }
-
-        /* Check for the "X^" */
-        if(!(stringScanner.hasNext("X^"))) {
-            return false;
-        } else {
-            /* Consume "X^" */
-            stringScanner.next("X^");
-        }
-
-        /* Check if there is an order */
-        if(!(stringScanner.hasNextInt())) {
-            return false;
-        } else {
-            /* Consume int */
-            stringScanner.nextInt();
-        }
-
-        return true;
-    }
-    public Polynomial stringToPoly(String s, int m) {
-
-        /* Split up and process the terms */
-        String parts[] = s.split("+");
-        Map<Integer, Integer> newPolyTerms = new HashMap();
-
-        for (String p:parts) {
-            /* Check if p's formatting is valid */
-            if (verifyPart(p)) {
-                Scanner partScanner = new Scanner(p);
-
-                int newCoefficient = partScanner.nextInt();
-                partScanner.next("X^");
-                int newOrder = partScanner.nextInt();
-
-                newPolyTerms.put(newCoefficient, newOrder);
-            } else {
-                /* Do error handling */
-            }
-        }
-
-        /* Create new polynomial and set it's terms to the new terms */
-        Polynomial newPoly = new Polynomial(m); /* TODO add prime */
-        newPoly.setTerms(newPolyTerms);
-        return newPoly;
-    }
-        
-        // three polynomials, where you decide if poly1 = poly2 mod poly3
-        // case1 is: "poly" poly1 poly2 poly3
-
-        // two polynomials mod prime, where you do sum, difference, product
-        // case2 is: "poly" poly1 poly2 prime
-        
-        // a multiplication coefficient , and one polynomial mod prime where you do the scalar multiplication
-        // case3 is: "poly" poly1 coefficient prime
- 
-        // two polynomials, mod prime, do xGCD
-        // case4 is: "poly" poly1 poly2 prime "gcd"
-        
-        // two polynomials, mod prime, do long division (return quotient and remainder)
-        // case5 is: "poly" poly1 poly2 prime "div"
-        
-        //prime number p and irreducible polynomial, return addition and multiplication table
-        //case1 is: "FF" prime poly1
-        
-        // two field elements a,b, field, return sum a+b, product ab, quotient ab^-1 if b!=0 
-        //case2 is: "FF" a b field
-        
-        //primitivity of field element
-        //case3 is: "FF" a field
-        
-        //find primitive elements in a field
-        //case4 is: "FF" field
-        
-        //test irreducibility of a poly mod p, return irreducible polynomials
-        //case5 is: "FF" poly modp   
 }
