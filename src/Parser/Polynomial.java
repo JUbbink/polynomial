6ea7b402508c4736f894a77f8fb941acfbca714a
JUbbink <ubbink.jeroen@gmail.com>
2015-10-26 23:51:45 +0100

    Parser fixed, doing math now

--- src/Parser/Polynomial.java
+++ src/Parser/Polynomial.java
@@ -51,7 +51,7 @@ public class Polynomial {
     
     public Polynomial(Map<Integer,Integer> a_terms){
         this.terms = new HashMap(a_terms);
-        this.modulus = 1;
+        this.modulus = 2147483646;//max value of an int
         this.updateModulo();
     }
     
@@ -59,28 +59,28 @@ public class Polynomial {
         Map<Integer,Integer> newMap = new HashMap();
         
         this.terms=newMap;
-        this.modulus = 1;
+        this.modulus = 2147483646;//max value of an int
     }
 
     /* Re-modulates all terms, to be called after every change */
     public void updateModulo() {
         /* for all terms, term = term % prime  */
         
-        Map<Integer,Integer> map = this.terms;
-        
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            int exp = entry.getKey();
+        for(Map.Entry<Integer,Integer> entry: this.terms.entrySet()){
             int coef = entry.getValue();
             
             coef = coef % modulus;
         
             /* Error checking for negative modulus */
-            if (coef < 0){
-                coef += modulus;
-            }
-            map.put(exp,coef);
+//            if (coef < 0){
+//                coef += modulus;
+            //}
+            entry.setValue(coef);
         }
-        this.terms = map;
+    }
+    
+    public int getMod(){
+        return this.modulus;
     }
     
     public String parsePoly(){
@@ -91,12 +91,13 @@ public class Polynomial {
         for (Map.Entry<Integer,Integer> entry: a_terms.entrySet()){
             String exp = entry.getKey().toString();
             String coef = entry.getValue().toString();  
-            output = output + exp+"^X"+coef+" + ";
+            output = output + coef + "X^" + exp + " + ";
         }
         output = output.substring(0,output.length()-3);
 //      System.out.println(output);
-        String reverse = new StringBuffer(output).reverse().toString();
-        return reverse;
+        //String reverse = new StringBuffer(output).reverse().toString();
+        //return reverse;
+        return output;
     }
 
     /* Accessor functions */
@@ -109,7 +110,7 @@ public class Polynomial {
         this.updateModulo();
     }
     
-    public void changeModulo(int n){
+    public void setModulo(int n){
         this.modulus = n;
         this.updateModulo();
     }
@@ -119,6 +120,9 @@ public class Polynomial {
         Map<Integer, Integer> poly2 = new HashMap(p.getTerms());
         
         Map<Integer,Integer> poly3 = new HashMap();
+        
+        int newMod = Math.max(this.modulus,p.modulus);
+        
 
         /* Adds the coefficients from the second polynomial to the first if the first has such polynomials */
         for (Map.Entry<Integer,Integer> entry: poly1.entrySet()) {
@@ -144,14 +148,13 @@ public class Polynomial {
         }
         /* takes all of the combined polynomials and overwrites conflicting terms in poly2, leaving added terms together with terms that didnt exist in poly1 */        
         poly2.putAll(poly3);
-        Polynomial pReturn = new Polynomial(poly2,this.modulus);
         /* Set the current coefficients to the added coefficients  */
-        return pReturn;
+        return new Polynomial(poly2,newMod);
     }
 
     public Polynomial divide(Polynomial p){
         /* TODO */
-        return this;
+        return new Polynomial();
     }
     
     public int[] gcd(Polynomial p){
@@ -160,16 +163,19 @@ public class Polynomial {
         return newArray;
     }
     
-    public void modulo(Polynomial p){
+    public Polynomial modulo(Polynomial p){
         /* TODO */
+        return new Polynomial();
     }
     
     public Polynomial multiply(Polynomial p){
         /* TODO */
-        Map<Integer,Integer> poly1 = this.getTerms();
-        Map<Integer,Integer> poly2 = p.getTerms();       
+        Map<Integer,Integer> poly1 = new HashMap(this.getTerms());
+        Map<Integer,Integer> poly2 = new HashMap(p.getTerms());       
         /* 1. Create a new Polynomial poly3                                      */
         /* 2. Find the polynomials which you want to multiply as poly1 and poly2 */
+
+        int newMod = Math.max(this.modulus,p.modulus);
 
         Map<Integer,Integer> poly3 = new HashMap();
 
@@ -184,8 +190,7 @@ public class Polynomial {
 		poly3.put(exponent, coefficient);
             }
         }
-        this.updateModulo();
-        return this;
+        return new Polynomial(poly3,newMod);
     }
     
     public Polynomial scalar(int s){
@@ -202,8 +207,8 @@ public class Polynomial {
     
     public Polynomial subtract(Polynomial p){
         /* TODO */
-        Map<Integer, Integer> poly1 = this.getTerms();
-        Map<Integer, Integer> poly2 = p.getTerms();
+        Map<Integer, Integer> poly1 = new HashMap(this.getTerms());
+        Map<Integer, Integer> poly2 = new HashMap(p.getTerms());
 
         /* Subtracts the coefficients from the second polynomial to the first if the first has such polynomials */
         for (Map.Entry<Integer,Integer> entry: poly1.entrySet()) {
@@ -212,13 +217,13 @@ public class Polynomial {
 
             /* Get the coefficients for both polynomials */
             
-            Integer poly1_check = poly1.get(key); /* TODO if no return, set to 0 */
+            Integer poly1_check = poly1.get(key); /* if no return, set to 0 */
             if (poly1_check == null){
                 poly1_check = 0;
             }
             int poly1_coef = poly1_check;
             
-            Integer poly2_check = poly2.get(key); /* TODO if no return, set to 0 */
+            Integer poly2_check = poly2.get(key); /* if no return, set to 0 */
             if (poly2_check == null){
                 poly2_check = 0;
             } 
@@ -230,9 +235,8 @@ public class Polynomial {
         /* takes all of poly1 and overwrites conflicting terms in poly2, leaving non-conflicting terms */
         poly2.putAll(poly1);
         /* Set the current coefficients to the added coefficients  */
-        this.setTerms(poly2);
-        this.updateModulo();
-        return this;
+        Polynomial pReturn = new Polynomial(poly2,this.modulus);
+        return pReturn;
     }
     
     public int[] xGCD(Polynomial p){
