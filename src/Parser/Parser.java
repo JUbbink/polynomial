9d5c64a8dbb1104bc963272e86e903fe94f23a4d
JUbbink <ubbink.jeroen@gmail.com>
2015-10-26 12:34:32 +0100

    Changed Polynomial Class, still no error checking, but the rest should be fine.

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -1,15 +1,12 @@
 package Parser;
 import java.util.Scanner;
 import java.io.*;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 import java.lang.*;
 import java.util.Map;
 import java.util.HashMap;
 
-/**
- *
- * @author Jeroen
+/*
+ @author Jeroen
  //TODO - define input parameters
  
  Types of inputs: 
@@ -32,7 +29,6 @@ import java.util.HashMap;
  != 0) in a field.
  
  single field element - 
- * 
  */
 public class Parser {
     
@@ -44,14 +40,14 @@ public class Parser {
     public void Parser(Scanner s){
         String l ="";
         
-        while(s.hasNext()==true){   //parsing input
+        while(s.hasNext() == true){   //parsing input
             l = s.nextLine();
         }
         String[] parts = l.split(" ");//this gets me an array of all the separate elements
         //List<String> items = Arrays.asList(str.split("\\s*,\\s*"));
 
         
-        if(parts.length == 5){//either case4 or 5
+        if(parts.length == 5){//either case 4 or 5
             int prime = Integer.parseInt(parts[3]);
             Polynomial poly1 = stringToPoly(parts[1],prime);
             Polynomial poly2 = stringToPoly(parts[2],prime);
@@ -59,14 +55,14 @@ public class Parser {
             if(parts[4].equals("div")){//case 5
                 //do division
                 poly1.divide(poly2);
-                System.out.println(parsePoly(poly1));
+                System.out.println(poly1.parsePoly());
             }
             else if(parts[4].equals("gcd")){//case 4
                 //do gcd
                 poly1.gcd(poly2);
-                System.out.println(parsePoly(poly1));
+                System.out.println(poly1.parsePoly());
                 poly1.xGCD(poly2);
-                System.out.println(parsePoly(poly1));
+                System.out.println(poly1.parsePoly());
             }
         }
         else if(parts.length==4){//remaining three cases
@@ -85,57 +81,30 @@ public class Parser {
                 Polynomial poly1 = new Polynomial(prime);
                 
                 poly1.scalar(coefficient);
-                System.out.println(parsePoly(poly1));
+                System.out.println(poly1.parsePoly());
             }
                 
             else if(verifyPart(parts[1]) && verifyPart(parts[2]) && !verifyPart(parts[3])){//only case 2 remains
                 int prime = Integer.parseInt(parts[3]);
-                Polynomial poly1 = new Polynomial(prime);
-                Polynomial poly2 = new Polynomial(prime);
+                Polynomial poly1 = stringToPoly(parts[1],prime);
+                Polynomial poly2 = stringToPoly(parts[2],prime);
                 
-                /* TODO: Fill HashMaps and setTerms */
                 
                 poly1.add(poly2);
-                System.out.println(parsePoly(poly1));
+                System.out.println(poly1.parsePoly());
                 
+                poly1 = stringToPoly(parts[1],prime);
+                poly2 = stringToPoly(parts[2],prime);
                 poly1.subtract(poly2);
-                System.out.println(parsePoly(poly1));
+                System.out.println(poly1.parsePoly());
                 
+                poly1 = stringToPoly(parts[1],prime);
+                poly2 = stringToPoly(parts[2],prime);
                 poly1.multiply(poly2);
-                System.out.println(parsePoly(poly1));
+                System.out.println(poly1.parsePoly());
             }
         }
-    }
-        
-//        else if(parts[0].equals("FF")){
-//            if(parts[1].matches("\\d")){//case 1,2,3,5
-//                if(parts[3].matches("a field")){//case 2
-//                    int a = Integer.parseInt(parts[1]);
-//                    int b = Integer.parseInt(parts[2]);
-//                    
-//                    //FiniteField field = new FiniteField();
-//                    
-//                    //return the sum, product, quotient ab^-1 if b!=0
-//                }
-//                else if(parts[2].contains(",")){//case1
-//                    int prime = Integer.parseInt(parts[1]);
-//                    Polynomial poly = new Polynomial();
-//                    
-//                    int count = parts[2].replaceAll("\\D", "").length();
-//                    for(int i = 0; i < count; i++){
-//                        poly.setCoefficient(Integer.parseInt((parts[2].charAt(i))+""));
-//                        poly.setOrder(count);
-//                    }
-//                    poly.create();
-//                    
-//                    //do case1
-//                }
-//            }
-//            else if(parts[1].matches("a field")){//case4
-//                //do case4
-//            }
-        
-            
+    }    
         //prime number p and irreducible polynomial, return addition and multiplication table
         //case1 is: "FF" prime poly
         
@@ -152,19 +121,6 @@ public class Parser {
         //case5 is: "FF" poly prime
 //        }
 //    }
-    
-    private String parsePoly(Polynomial p){
-        String output = "";
-        Map<Integer, Integer> terms = p.getTerms();
-        
-        for (Map.Entry<Integer,Integer> entry: terms.entrySet()){
-            String key = entry.getKey().toString();
-            String value = entry.getValue().toString();  
-            output = output + key + "X^" + value + " + ";  
-        } 
-//      System.out.println(output);
-        return output;
-    }
     
     private boolean verifyPart(String s) {
         Scanner stringScanner = new Scanner(s);
@@ -250,29 +206,5 @@ public class Parser {
         //case4 is: "FF" field
         
         //test irreducibility of a poly mod p, return irreducible polynomials
-        //case5 is: "FF" poly modp
-        
-        
-//        else if(parts[0]== "FF"){
-//            //TODO - Finite Field parsing
-//            if(parts.length == 2){
-//                //case4
-//            }
-//            else if(parts[2].matches("*mod*")){
-//                //case5
-//            }
-//            else if(parts[2].matches("*X*")){
-//                //case1
-//            }
-//            else if(parts[2].substring(0, 1).matches("[0-9]") && parts[2].length()==1){
-//                //case2
-//            }
-//            else if(parts[2]=="a field"){ //TODO defining a field field/polynomial
-//                //case3
-//            }
-//            
-//        }
-        
- //   }
-    
+        //case5 is: "FF" poly modp   
 }
