29107ddf697c0850340823413d100c3a643404dd
Jeroen Ubbink <ubbink.jeroen@gmail.com>
2016-10-27 16:28:51 +0200

    FiniteField, need to add robustness, and 4 methods. String representation of Finite Field is most important.

--- src/Parser/FiniteField.java
+++ src/Parser/FiniteField.java
@@ -5,7 +5,9 @@
  */
 package Parser;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -43,47 +45,75 @@ public class FiniteField {
             int r = i % modulo;
             int q = i / modulo;
             for (int j = 0; j <= deg; j++) {
-                //missing step here
+                Map testMap = new HashMap();
+                testMap.put(r % modulo, j);
+                Polynomial test = new Polynomial(testMap, modulo);
+                element = element.add(test);
                 r = q % modulo;
                 q = q / modulo;
             }
-            elements.add(element);
+            Polynomial[] divisionResult = element.divide(g);
+            elements.add(divisionResult[1]);
         }
     }
 
+    //NOT DONE
     public String additionTable() {
         //TODO: Return a string
         return "";
     }
 
+    //NOT DONE
     public String multiplicationTable() {
         return "";
     }
-
+    
+    //DONE
     public Polynomial sum(Polynomial p1, Polynomial p2) {
-        Polynomial result = p1.add(p2);
-        //TODO: ADD MODULO SOME POLY
-        return new Polynomial();
+        Polynomial poly = p1.add(p2);
+        Polynomial[] result = poly.divide(p2);
+        return result[1];
     }
-
+    
+    //DONE
     public Polynomial product(Polynomial p1, Polynomial p2) {
-        Polynomial result = p1.multiply(p2);
-        //TODO: ADD MODULO SOME POLY
-        return new Polynomial();
-
+        Polynomial poly = p1.multiply(p2);
+        Polynomial[] result = poly.divide(p2);
+        return result[1];
     }
-
+    
+    //DONE
     public Polynomial quotient(Polynomial p1, Polynomial p2) {
-        Polynomial result = p1.multiply(p2);
-        return new Polynomial();
+        Polynomial result = p1.multiply(inverse(p2));
+        return result;
 
     }
-
+ 
+    //NOT DONE
     public boolean isPrimitive(Polynomial p) {
         return false;
     }
 
+    //NOT DONE
     public Polynomial primitive() {
+        return new Polynomial();
+    }
+
+    private Polynomial inverse(Polynomial p) {
+        //TODO Add checking if p is in finite field, AND some error polynomial
+
+        int m = this.generator.getMod();
+        Polynomial[] output = this.generator.xGCD(p);
+
+        Map terms = new HashMap();
+        terms.put(0, 1);
+        Polynomial unitPoly = new Polynomial(terms, m);
+        if (output[0] == unitPoly) {
+            Polynomial[] poly = output[0].divide(this.generator);
+             if (elements.contains(poly[1])){
+                 return poly[1];
+             }
+        }
         return new Polynomial();
     }
 }
